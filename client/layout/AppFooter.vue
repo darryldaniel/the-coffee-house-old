ea02c3becf3905680d19ab7acf10b205eba25b9d
Darryl Daniel <darrylndaniel@gmail.com>
2018-08-14 13:13:15 +0200

    feat (theme): add bulmaswatch minty theme and redo admin form

--- client/layout/AppFooter.vue
+++ client/layout/AppFooter.vue
@@ -1,9 +1,18 @@
 <template>
   <footer class="footer">
     <div class="content has-text-centered">
-      <p>Made with <span class="icon is-small has-text-danger">
+      <p>Made with <span class="icon is-small footer__heart-icon">
         <i class="fas fa-heart"></i>
       </span> and <span class="has-text-success has-text-weight-bold">Vue</span>. Hosted on <span class="has-text-black has-text-weight-bold is-uppercase">zeit</span>.</p>
     </div>
   </footer>
 </template>
+
+<style lang="scss" scoped>
+.footer {
+  &__heart-icon {
+    color: #f40d0d;
+  }
+}
+</style>
+
